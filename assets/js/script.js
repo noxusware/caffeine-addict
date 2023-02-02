38dd9c89e01c7122d392b5f507f992341d6e31a3
Vipul Patil <vipulrpatil.8@gmail.com>
2023-02-03 03:20:10 +0530

    css minified and js optimized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,44 @@
 'use strict';
-
-
 $( document ).ready(function() {
-  console.log('In Jquery')
   $("#tabs").tabs({
     active:0
   })
 
 });
-
 function togglePopup(name){
   $(".popup").each(function() {
     $(this).css('display','none')
   });
-  
-  
+ 
   if(name)
     $(`.popup[name="${name}"]`).css('display','flex')
 
   $(".popup-container").toggle();
 
 }
-
-// add event on multiple elements
-
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
-
-
-
-// navbar functionality
-
 const [navbar, navToggler, navbarLinks] = [
   document.querySelector("[data-navbar]"),
   document.querySelector("[data-nav-toggler]"),
   document.querySelectorAll("[data-nav-link]")
 ];
-
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
   document.body.classList.toggle("active");
 }
-
 navToggler.addEventListener("click", toggleNavbar);
-
-
 const closeNavbar = function () {
   navbar.classList.remove("active");
   navToggler.classList.remove("active");
   document.body.classList.remove("active");
 }
-
 addEventOnElements(navbarLinks, "click", closeNavbar);
-
-
-
-// header active
-
 const header = document.querySelector("[data-header]");
-
 const activeElemOnScroll = function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
@@ -71,33 +46,19 @@ const activeElemOnScroll = function () {
     header.classList.remove("active");
   }
 }
-
 window.addEventListener("scroll", activeElemOnScroll);
-
-
-
-// scroll reveal effect
-
 const revealElements = document.querySelectorAll("[data-reveal]");
-
 const revealOnScroll = function () {
   for (let i = 0; i < revealElements.length; i++) {
-
-    // add revealed class on element, when visible in window
     if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1) {
       revealElements[i].classList.add("revealed");
-
-      // remove long transition from button, after 1 second
       if (revealElements[i].classList.contains("btn")) {
         setTimeout(function () {
           revealElements[i].style.transition = "0.25s ease";
         }, 1000);
       }
     }
-
   }
 }
-
 window.addEventListener("scroll", revealOnScroll);
-
 revealOnScroll();
